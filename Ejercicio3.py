75158bfafbe10c1badd150b847c5562b5bfccfce
Jojo3235 <113980747+Jojo3235@users.noreply.github.com>
2022-09-20 21:53:02 +0000

    ajuste en ejercicio 3

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,7 +1,7 @@
 #Ejercicio 3
 username = str(input("Esribe tu nombre de usuario: "))        #Creamos una entrada para definir las variables
 password = str(input("Escribe tu Contraseña: "))
-if len(username) >= 3:                                        #Creamos una condición para que nos devuelva que la contraseña es True o False
+if len(username) > 3 and len(username)<10:                                        #Creamos una condición para que nos devuelva que la contraseña es True o False
   print("Usuario: True")                                      #Si la longitud de la palabra dentro de usuario es mayor a 3, entonces devuelve True
 else:                                               
   print("Usuario: False")                                     #Si no nos devuelve False
