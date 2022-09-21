04060f0c50bc0abb8ab7769d471c9ed81ef964e8
Jojo3235 <113980747+Jojo3235@users.noreply.github.com>
2022-09-21 08:13:02 +0000

    Eliminación de los condicionales en el ejercicio 3

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,11 +1,5 @@
 #Ejercicio 3
 username = str(input("Esribe tu nombre de usuario: "))        #Creamos una entrada para definir las variables
 password = str(input("Escribe tu Contraseña: "))
-if len(username) > 3 and len(username)<10:                                        #Creamos una condición para que nos devuelva que la contraseña es True o False
-  print("Usuario: True")                                      #Si la longitud de la palabra dentro de usuario es mayor a 3, entonces devuelve True
-else:                                               
-  print("Usuario: False")                                     #Si no nos devuelve False
-if password == "Tokio" or "Python":                           #Lo mismo para la contraseña, pero en vez de la longitud, tiene que ser igual a Tokio o Python
-  print("password: True")
-else:
-  print("password: False")
+print(3<len(username)<10)                                     #Imprimimos si username es mayor que 3 o menor que 10, devuelve True o False
+print(password == "Tokio" or password == "Python")            #Imprimimos si password es Tokio o Python, devuelve True o False
--- main.py
+++ main.py
@@ -10,14 +10,8 @@ print(round(operacion,2))                                     #Imprimimos la ope
 #Ejercicio 3
 username = str(input("Esribe tu nombre de usuario: "))        #Creamos una entrada para definir las variables
 password = str(input("Escribe tu Contraseña: "))
-if len(username) >= 3:                                        #Creamos una condición para que nos devuelva que la contraseña es True o False
-  print("Usuario: True")                                      #Si la longitud de la palabra dentro de usuario es mayor a 3, entonces devuelve True
-else:                                               
-  print("Usuario: False")                                     #Si no nos devuelve False
-if password == "Tokio" or "Python":                           #Lo mismo para la contraseña, pero en vez de la longitud, tiene que ser igual a Tokio o Python
-  print("password: True")
-else:
-  print("password: False")
+print(3<len(username)<10)                                     #Imprimimos si username es mayor que 3 o menor que 10, devuelve True o False
+print(password == "Tokio" or password == "Python")            #Imprimimos si password es Tokio o Python, devuelve True o False
 
 #Ejercicio 4
 num1=3;num2=15                                                      #Definimos los dos números como variables
