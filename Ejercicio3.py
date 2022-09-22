75a3cc3f97840ca698795eaf87030ea51f145466
Jojo3235 <113980747+Jojo3235@users.noreply.github.com>
2022-09-22 17:43:34 +0000

    ajuste comentarios

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,5 +1,5 @@
 #Ejercicio 3
 username = str(input("Esribe tu nombre de usuario: "))        #Creamos una entrada para definir las variables
 password = str(input("Escribe tu Contraseña: "))
-print(3<len(username)<10)                                     #Imprimimos si username es mayor que 3 o menor que 10, devuelve True o False
+print(3<len(username)<10)                                     #Imprimimos si el username es mayor que 3 o menor que 10
 print(password == "Tokio" or password == "Python")            #Imprimimos si password es Tokio o Python, devuelve True o False
